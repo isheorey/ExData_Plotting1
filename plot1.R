0d985c99fcf66762e5a6f35e58651151ed9432b7
isheorey <indraneelsheorey@gmail.com>
2014-10-12 17:34:31 -0700

    Updated data cleanup and plot labeling

--- plot1.R
+++ plot1.R
@@ -21,9 +21,48 @@ unzip("data/household_power_consumption.zip", exdir = "data")
 message("Reading in dataset...")
 hpc <- read.table("data/household_power_consumption.txt",
                   header = TRUE,
-                  sep = ";")
+                  sep = ";",
+                  colClasses = rep("character", 9))
 
 # Select relevant data for time period
-hpc.short <- hpc[as.Date(hpc$Date) >= '2002-02-01' &
-                     as.Date(hpc$Date) <= '2002-02-02', ]
-hist(hpc.short$Global_active_power, col = "red")
+library(lubridate)
+hpc$Date <- dmy(hpc$Date)
+hpc$Time <- hms(hpc$Time)
+hpc.short <- hpc[hpc$Date > "2007-01-31" &
+                     hpc$Date < "2007-02-02", ]
+
+# Convert data types
+hpc.short$Global_active_power <-
+    as.numeric(ifelse(hpc.short$Global_active_power == "?",
+                      NA,
+                      hpc.short$Global_active_power))
+hpc.short$Global_reactive_power <-
+    as.numeric(ifelse(hpc.short$Global_reactive_power == "?",
+                      NA,
+                      hpc.short$Global_reactive_power))
+hpc.short$Voltage <-
+    as.numeric(ifelse(hpc.short$Voltage == "?",
+                      NA,
+                      hpc.short$Voltage))
+hpc.short$Global_intensity <-
+    as.numeric(ifelse(hpc.short$Global_intensity == "?",
+                      NA,
+                      hpc.short$Global_intensity))
+hpc.short$Sub_metering_1 <-
+    as.numeric(ifelse(hpc.short$Sub_metering_1 == "?",
+                      NA,
+                      hpc.short$Sub_metering_1))
+hpc.short$Sub_metering_2 <-
+    as.numeric(ifelse(hpc.short$Sub_metering_2 == "?",
+                      NA,
+                      hpc.short$Sub_metering_2))
+hpc.short$Sub_metering_3 <-
+    as.numeric(ifelse(hpc.short$Sub_metering_3 == "?",
+                      NA,
+                      hpc.short$Sub_metering_3))
+
+# Plot data
+hist(hpc.short$Global_active_power,
+     main = "Global Active Power",
+     col = "red",
+     xlab = "Global Active Power (kilowatts)")
